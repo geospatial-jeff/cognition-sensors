69d653a5399917dbee7c324667cd2338efe63905
Jeff Albrecht <jeff.albrecht@slingshotaerospace.com>
2018-11-26 12:10:31 -0700

    fixing name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cognition-sensors",
+    name="sensors",
     version="0.1",
     description="Sensor configurations and patterns for automatic ingest",
     author="Jeff Albrecht",
