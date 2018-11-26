73a34f872e06efb67ca58d6650d8db5a61ada250
Jeff Albrecht <jeff.albrecht@slingshotaerospace.com>
2018-11-26 12:14:18 -0700

    raise error if pattern doesn't match

--- sensors/library.py
+++ sensors/library.py
@@ -6,6 +6,8 @@ ls7_ard = re.compile(r"^L(\S{1})(07)_(\S{2})_(\d{6})_(\d{8})_(\d{8})_(\w{3})_(\w
 ls8_ard = re.compile(r"^L(\S{1})(08)_(\S{2})_(\d{6})_(\d{8})_(\d{8})_(\w{3})_(\w{3})_(\w+)(\w{2}\.\S{3})")
 ls8 = re.compile(r"^L(\S{1})(08)_(\w{4})_(\d{6})_(\d{8})_(\d{8})_(\d{2})_(\w{2})")
 
+class InvalidPattern(BaseException):
+    pass
 
 class AssetLibrary(object):
 
@@ -27,6 +29,7 @@ class Asset(AssetLibrary):
             match = regex.match(self.fname)
             if match is not None:
                 return self.regexes[regex](match.groups())
+        raise InvalidPattern("Filename does not match any sensors in the library")
 
     def metadata(self):
         return self.sensor.metadata()
