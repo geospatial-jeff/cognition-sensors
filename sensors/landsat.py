fc8949817a70e04d32488b7a1443de93b258e494
Jeff Albrecht <jeff.albrecht@slingshotaerospace.com>
2018-11-26 12:01:12 -0700

    Moved from cognition-client

---
+++ sensors/landsat.py
@@ -0,0 +1,83 @@
+from datetime import datetime
+
+class Landsat(object):
+
+    """https://landsat.usgs.gov/landsat-collections"""
+
+    def __init__(self, parts):
+        self.parts = list(parts)
+        self.labels = ['sensor', 'satellite']
+        self.lut = [{'C': 'OLI/TIRS',
+                     'O': 'OLI',
+                     'E': 'ETM+',
+                     'T': 'TM',
+                     'M': 'MSS'
+                     },
+                    {'07': 'Landsat7',
+                     '08': 'Landsat8',
+                     },
+                    ]
+
+    def metadata(self):
+        tile_numbers = self.parts.pop(3)
+        d = {'wrs_path': tile_numbers[:3],
+             'wrs_row': tile_numbers[3:],
+             'acquisition_date': datetime.strptime(self.parts.pop(3), '%Y%m%d'),
+             'production_date': datetime.strptime(self.parts.pop(3), '%Y%m%d'),
+             "collection_number": self.parts.pop(3),
+             "collection_category": self.parts.pop(3),
+             "processing_level": self.parts.pop(-1)}
+        for idx, item in enumerate(self.lut):
+            d.update({self.labels[idx]: self.lut[idx][self.parts.pop(0)]})
+        return d
+
+
+class Landsat_ARD(object):
+
+    """https://landsat.usgs.gov/ard"""
+
+    def __init__(self, parts):
+        self.parts = list(parts)
+        self.labels = ["sensor", "satellite", "regional_grid", "product"]
+        self.lut = [{'T': 'TM',
+                     'E': 'ETM',
+                     'C': 'OLI/TIRS',
+                     'O': 'OLI',
+                     },
+                    {'04': 'Landsat4',
+                     '05': 'Landsat5',
+                     '07': 'Landsat7',
+                     '08': 'Landsat8'
+                     },
+                    {'CU': 'CONUS',
+                     'AK': 'Alaska',
+                     'HI': 'Hawaii'
+                     },
+                    {'TA': 'top of atmosphere reflectance',
+                     'BT': 'brightness temperature',
+                     'SR': 'surface reflectance',
+                     'ST': 'land surface temperature',
+                     'SOA': 'solar azimuth angle',
+                     'SOZ': 'solar zenith angle',
+                     'SEA': 'sensor azimuth angle',
+                     'SEZ': 'sensor zenith angle',
+                     'PIXELQA': 'pixel quality attributes',
+                     'RADSATQA': 'radiometric saturation',
+                     'LINEAGEQA': 'lineage index',
+                     'SRATMOSOPACITYQA': 'internal landsat 4-7 surface reflectance atmospheric opacity',
+                     'SRCLOUDQA': 'internal Landsat 4-7 surface reflectane quality',
+                     'SRAEROSOLQA': 'internal Landsat 8 surface reflectance aerosol parameters'}]
+
+    def metadata(self):
+        tile_numbers = self.parts.pop(3)
+        d = {'horizontal_tile_number': tile_numbers[:3],
+             'vertical_tile_number': tile_numbers[3:],
+             'acquisition_date': datetime.strptime(self.parts.pop(3), '%Y%m%d'),
+             'production_date': datetime.strptime(self.parts.pop(3), '%Y%m%d'),
+             'collection_number': self.parts.pop(3),
+             'ard_version': self.parts.pop(3),
+             'band': self.parts.pop(-1)[1]
+             }
+        for idx, item in enumerate(self.lut):
+            d.update({self.labels[idx]: self.lut[idx][self.parts.pop(0)]})
+        return d
---
+++ sensors/library.py
@@ -0,0 +1,32 @@
+import re
+
+from cognition.sensors.landsat import Landsat_ARD, Landsat
+
+ls7_ard = re.compile(r"^L(\S{1})(07)_(\S{2})_(\d{6})_(\d{8})_(\d{8})_(\w{3})_(\w{3})_(\w+)(\w{2}\.\S{3})")
+ls8_ard = re.compile(r"^L(\S{1})(08)_(\S{2})_(\d{6})_(\d{8})_(\d{8})_(\w{3})_(\w{3})_(\w+)(\w{2}\.\S{3})")
+ls8 = re.compile(r"^L(\S{1})(08)_(\w{4})_(\d{6})_(\d{8})_(\d{8})_(\d{2})_(\w{2})")
+
+
+class AssetLibrary(object):
+
+    def __init__(self):
+        self.regexes = {ls8: Landsat,
+                        ls7_ard: Landsat_ARD,
+                        ls8_ard: Landsat_ARD
+                        }
+
+class Asset(AssetLibrary):
+
+    def __init__(self, fname):
+        AssetLibrary.__init__(self)
+        self.fname = fname
+        self.sensor = self.lookup()
+
+    def lookup(self):
+        for regex in self.regexes:
+            match = regex.match(self.fname)
+            if match is not None:
+                return self.regexes[regex](match.groups())
+
+    def metadata(self):
+        return self.sensor.metadata()
