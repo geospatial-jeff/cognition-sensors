737de57ef208f708f34002e8306352fd042080ab
Jeff Albrecht <jeff.albrecht@slingshotaerospace.com>
2018-12-08 08:33:27 -0700

    adding requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("./requirements.txt") as reqs:
+    requirements = [line.rstrip() for line in reqs if "git" not in line]
+
 setup(
     name="sensors",
     version="0.1",
@@ -7,4 +10,5 @@ setup(
     author="Jeff Albrecht",
     author_email="geospatialjeff@gmail.com",
     packages=find_packages(),
+    install_requires=requirements
 )
